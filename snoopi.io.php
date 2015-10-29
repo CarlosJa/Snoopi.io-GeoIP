e127ae29f047cdf991cd05b33433b2d438a16f7d
Carlos Arias <webmaster@carlosja.com>
2015-10-29 13:11:58 -0400

    Adding Snoopi.io PHP
    Initial PHP Script to work with Snoopi.io webservice.

---
+++ snoopi.io.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * Website http://www.snoopi.io
+ * Author: carlos@snoopi.io CarlosJaa
+ * Any bugs, question, please email carlos@snoopi.io
+ */
+
+class Snoopi { 
+
+private $api_key = 'API_KEY';
+
+
+	function GeoIPLocation($IPAddress) {
+		$QueryArr['apikey']=$this->api_key;
+		$QueryArr['user_ip_address']=$IPAddress;
+
+
+		$URLQuery =  urldecode(http_build_query($QueryArr, '', '&'));
+
+
+		// create curl resource
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, "http://api.snoopi.io/v1/?" . $URLQuery);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		$output = curl_exec($ch);
+		curl_close($ch);
+
+
+		return $output;
+	}
+
+
+
+
+} // End Class
